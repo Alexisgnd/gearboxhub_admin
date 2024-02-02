c40fb00da2001fd89f16da07f29bf8719c997d82
Alexis GONNEAUD <alexis.gonneaud@hotmail.fr>
2024-02-02 18:51:13 +0100

    Add : added billboards entity

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,8 +1,5 @@
-import { format } from "date-fns";
-
 import prismadb from "@/lib/prismadb";
 
-import { BillboardColumn } from "./components/columns"
 import { BillboardClient } from "./components/client";
 
 const BillboardsPage = async ({
@@ -19,16 +16,10 @@ const BillboardsPage = async ({
     }
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
-    id: item.id,
-    label: item.label,
-    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-  }));
-
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards} />
+        <BillboardClient data={billboards} />
       </div>
     </div>
   );
